3f113322ee15e97ccd56867b2042a53768e78e3d
Marllon <99225101+marllonlaborne@users.noreply.github.com>
2023-03-03 20:41:14 -0300

    animação ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -2,26 +2,46 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.halfWindow = window.innerHeight * 0.6;
+    this.activeClass = 'ativo';
 
-    // Bind
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const activeClass = 'ativo';
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.halfWindow) < 0;
-      if (isSectionVisible) {
-        section.classList.add(activeClass);
-      } else if (section.classList.contains(activeClass)) {
-        section.classList.remove(activeClass);
+  // Pega a distância de cada item
+  // em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map(section => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfWindow),
+      };
+    });
+  }
+
+  // Verifica a distância em cada objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach(item => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add(this.activeClass);
+      } else if (item.element.classList.contains(this.activeClass)) {
+        item.element.classList.remove(this.activeClass);
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove eventListener de scroll
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
